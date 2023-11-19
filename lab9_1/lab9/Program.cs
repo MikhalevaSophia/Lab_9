904c762a0300fc482706a6fdb1a43a6e7901cb86
Sophia <112760887+MikhalevaSophia@users.noreply.github.com>
2023-11-19 20:44:56 +0300

    Add files via upload

---
+++ lab9_1/lab9/Program.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Data;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
+namespace lab9
+{
+    class Program
+    {
+        static readonly Mutex mutex = new Mutex();
+
+        static async Task Main()
+        {
+            List<string> list = new List<string>();
+
+            using (StreamReader reader = new StreamReader("ticker.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+            using (HttpClient client = new HttpClient())
+            {
+                List<Task> tasks = new List<Task>();
+                foreach (string ticker in list)
+                {
+                    tasks.Add(GetDataForTickets(client, ticker));
+                }
+                await Task.WhenAll(tasks);
+            }
+        }
+        static async Task GetDataForTickets(HttpClient client, string ticker)
+        {
+            try
+            {
+                DateTime sdate = DateTime.Now.AddYears(-1);
+                DateTime edate = DateTime.Now;
+
+                long sunixdate = ((DateTimeOffset)sdate).ToUnixTimeSeconds();
+                long eunixdate = ((DateTimeOffset)edate).ToUnixTimeSeconds();
+
+                string url = $"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={sunixdate}&period2={eunixdate}&interval=1d&events=history&includeAdjustedClose=true\r\n";
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string csvdata = await response.Content.ReadAsStringAsync();
+
+                string[] strings = csvdata.Split('\n');
+                double totalAveragePrice = 0.0;
+                int totalRowPrice = 0;
+
+                for (int i = 1; i < strings.Length - 1; i++)
+                {
+                    try
+                    {
+                        string[] values = strings[i].Split(',');
+
+                        double high = Convert.ToDouble(values[2], new System.Globalization.CultureInfo("en-US"));
+                        double low = Convert.ToDouble(values[3], new System.Globalization.CultureInfo("en-US"));
+                        double averagePrice = (high + low) / 2;
+
+                        totalAveragePrice += averagePrice;
+                        totalRowPrice++;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                if (totalRowPrice > 0)
+                {
+                    double totalAverage = totalAveragePrice / totalRowPrice;
+                    string result = $"{ticker}:{totalAverage}";
+
+                    mutex.WaitOne();
+                    try
+                    {
+                        File.AppendAllText("result.txt", result + Environment.NewLine);
+                    }
+                    finally
+                    {
+                        mutex.ReleaseMutex();
+                    }
+                    Console.WriteLine($"Средняя цена акции для {ticker} за год: {totalAverage}");
+                }
+                else
+                {
+                    Console.WriteLine($"Для {ticker} нет данных за год.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine ($"Ошибка при обработке {ticker}: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ lab9_2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grafica
+{
+    public struct City
+    {
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public struct Weather
+    {
+        public string Country { get; set; }
+        public string Name { get; set; }
+        public double Temp { get; set; }
+        public string Description { get; set; }
+    }
+
+    public partial class Form1 : Form
+    {
+
+        City _city;
+        static String _string = "Press Button Again";
+        static bool flag = false;
+
+        static string API_KEY = "e1991dfa974e7f364760706f2f4ae85e";
+
+        public class API_call
+        {
+            public static async Task<Weather> GetWeather(City city)
+            {
+
+                var url = $"https://api.openweathermap.org/data/2.5/weather";
+                var parameters = $"?lat={city.Latitude}&lon={city.Longitude}&appid={API_KEY}";
+
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(url);
+
+                // Запрос, пока не получим ответ
+                HttpResponseMessage response = await client.GetAsync(parameters).ConfigureAwait(false);
+
+                Console.WriteLine("Fetching data...");
+                try
+                {
+                    response = await client.GetAsync(parameters);
+                }
+                catch (System.Net.Http.HttpRequestException)
+                {
+                    Console.WriteLine($"{city.Name} : Connection troubles, try again");
+                }
+
+                // Считываем ответ как стрингу
+                Weather result = new Weather();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string rawResponse = await response.Content.ReadAsStringAsync();
+
+                    Regex rx = new Regex("(?<=\"country\":\")[^\"]+(?=\")");
+                    result.Country = rx.Match(rawResponse).ToString();
+                    rx = new Regex("(?<=\"name\":\")[^\"]+(?=\")");
+                    result.Name = rx.Match(rawResponse).ToString();
+                    rx = new Regex("(?<=\"temp\":)[^\"]+(?=,)");
+                    result.Temp = Math.Round(Convert.ToDouble(rx.Match(rawResponse).ToString()) - 273);
+                    rx = new Regex("(?<=\"description\":\")[^\"]+(?=\")");
+                    result.Description = rx.Match(rawResponse).ToString();
+
+                }
+
+                // Если смогли достать хоть какие-то данные, считаем среднее. Иначе - возвращаем null.
+                //MessageBox.Show(city.Name + ": " + result.Temp.ToString() + " degrees, " + result.Description);
+
+                _string = city.Name + ": " + result.Temp.ToString() + " degrees, " + result.Description;
+                flag = true;
+                return result;
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            CultureInfo.CurrentCulture = new CultureInfo("en-US");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.AddRange(File.ReadAllLines("C:\\Users\\mi\\source\\repos\\LR9_C\\city.txt"));
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string item = listBox1.SelectedItem.ToString();
+
+
+            var line = item.Split(new String[] { "\t", ",", " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            City newCity = new City();
+            newCity.Name = line[0];
+
+            for (int i = 1; i < line.Length - 2; i++)
+                newCity.Name += $" {line[i]}";
+
+            newCity.Latitude = Convert.ToDouble(line[line.Length - 2]);
+            newCity.Longitude = Convert.ToDouble(line[line.Length - 1]);
+            _city = newCity;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                API_call.GetWeather(_city);
+            });
+
+            while (!flag) { };
+            listBox2.Items.Clear();
+            listBox2.Items.Insert(0, _string);
+            flag = false;
+        }
+    }
+}
